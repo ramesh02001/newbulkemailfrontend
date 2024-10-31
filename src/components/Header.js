5f15f6e5011e883f3297c3d549cbcdbda6bfa65d
ramesh02001 <137371718+ramesh02001@users.noreply.github.com>
2024-10-31 20:55:04 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,11 +30,11 @@ function Header() {
                     {/* <p className='navbar-link'><Link to='/contact'>Contacts</Link></p> */}
                     <p className='navbar-link'><Link exact to='/login'>Login</Link></p>
                     <p className='navbar-link'><Link to='/signup'>Sign Up</Link></p>
-                    {/* <p className='navbar-link'><Link to='/account'>My Account</Link></p>*/}
-                    {/*<p className='navbar-link'><Link to='/adduser'>Add User</Link></p>*/}
-                   {/*<p className='navbar-link'><Link to='/edit/:id'>Edit</Link></p>*/}
+                     <p className='navbar-link'><Link to='/account'>My Account</Link></p>
+                    {/* <p className='navbar-link'><Link to='/adduser'>Add User</Link></p>
+                    {/*<p className='navbar-link'><Link to='/edit/:id'>Edit</Link></p>*/}
                     {/* <p className='navbar-link'><Link to='/forgot-password'>Forgot Password</Link></p>
-                     {/*<p className='navbar-link'><Link to='/reset-password/:token'>Reset Password</Link></p> */}
+                     <p className='navbar-link'><Link to='/reset-password/:token'>Reset Password</Link></p> */}
                     <p className='navbar-link'>  <Link onClick={handleLogout} className='navbar-btn btn-logout'>Logout</Link></p>
                 </div>
                 <div onClick={showNavbar} className='navbar-menu-toggle'>
